6cadbef9bb05b6376b1ccc4ecf2a67f9e44e13c2
Dave Matthews <support@pressjitsu.com>
2016-01-26 14:25:14 +0000

    Create redis-page-cache.php

---
+++ redis-page-cache.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Redis Page Cache
+ * Plugin URI: https://pressjitsu.com
+ * Description: Redis-backed full page caching plugin for WordPress.
+ * Version: 0.8
+ * License: GPLv3 or later
+ *
+ * To install create a symbolic link to the advanced-cache.php dropin from
+ * your wp-content directory and enable page caching in your wp-config.php file.
+ *
+ * Refer to the following URL for further instructions:
+ *
+ * https://github.com/pressjitsu/redis-page-cache/
+ *
+ * Please keep this helper plugin active for update notifications and future
+ * helper and CLI functionality.
+ */
